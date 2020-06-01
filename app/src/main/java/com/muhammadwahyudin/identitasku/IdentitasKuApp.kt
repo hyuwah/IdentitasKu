c24a6a36d563e951be653e3aa34565a3f238d69a
Muhammad Wahyudin <muhammad.whydn@gmail.com>
2020-06-01 14:55:43 +0700

    maintenance: update crashlytics, gradle plugin & several dependencies

--- app/src/main/java/com/muhammadwahyudin/identitasku/IdentitasKuApp.kt
+++ app/src/main/java/com/muhammadwahyudin/identitasku/IdentitasKuApp.kt
@@ -2,14 +2,13 @@ package com.muhammadwahyudin.identitasku
 
 import android.util.Log
 import androidx.multidex.MultiDexApplication
-import com.crashlytics.android.Crashlytics
 import com.facebook.stetho.Stetho
 import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.muhammadwahyudin.identitasku.di.appModule
 import com.muhammadwahyudin.identitasku.di.dataModule
 import com.orhanobut.hawk.Hawk
 import com.yariksoffice.lingver.Lingver
-import io.fabric.sdk.android.Fabric
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import timber.log.Timber
@@ -23,7 +22,6 @@ class IdentitasKuApp : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
         Hawk.init(this).build()
-        Fabric.with(this, Crashlytics())
         FirebaseAnalytics.getInstance(this)
         if (BuildConfig.DEBUG) {
             Timber.plant(DebugTree())
@@ -43,17 +41,17 @@ class IdentitasKuApp : MultiDexApplication() {
     }
 
     inner class CrashReleaseTree : Timber.Tree() {
-        val CRASHLYTICS_KEY_PRIORITY = "priority"
-        val CRASHLYTICS_KEY_TAG = "tag"
-        val CRASHLYTICS_KEY_MESSAGE = "message"
+        private val CRASHLYTICS_KEY_PRIORITY = "priority"
+        private val CRASHLYTICS_KEY_TAG = "tag"
+        private val CRASHLYTICS_KEY_MESSAGE = "message"
 
         override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
             if (priority == Log.VERBOSE || priority == Log.DEBUG) return
             val throwable = t ?: Exception(message)
-            Crashlytics.setInt(CRASHLYTICS_KEY_PRIORITY, priority)
-            Crashlytics.setString(CRASHLYTICS_KEY_TAG, tag)
-            Crashlytics.setString(CRASHLYTICS_KEY_MESSAGE, message)
-            Crashlytics.logException(throwable)
+            FirebaseCrashlytics.getInstance().setCustomKey(CRASHLYTICS_KEY_PRIORITY, priority)
+            FirebaseCrashlytics.getInstance().setCustomKey(CRASHLYTICS_KEY_TAG, tag.orEmpty())
+            FirebaseCrashlytics.getInstance().setCustomKey(CRASHLYTICS_KEY_MESSAGE, message)
+            FirebaseCrashlytics.getInstance().recordException(throwable)
         }
     }
 }
